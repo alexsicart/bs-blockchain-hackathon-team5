7660f1e12912497769f6bb8b7e4aebc2c4008b3d
Alex Sicart Ramos <alexsicart@MacBook-Pro-de-Alex.local>
2017-02-27 17:01:59 +0100

    last changes

--- booking/migrations/2_deploy_contracts.js
+++ booking/migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Token = artifacts.require("./Token.sol");
+var BSToken = artifacts.require("./BSToken.sol");
+var BSToken = artifacts.require("./Booking.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Token);
+  deployer.deploy(BSToken);
+  deployer.deploy(Booking);
 };
